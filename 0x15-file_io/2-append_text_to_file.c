24a5853d65ba71b067bb33eeb634d52f35b476dd
Lesego0773 <kabelomakokga@gmail.com>
2023-12-13 10:19:27 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,31 +1,36 @@
 #include "main.h"
 #include "stdlib.h"
 
-/*
- *append_text_to_file - appending the text to the existing file. 
- *@file_D - a method used to read only file and append.
- *text_content - checking each letter by using a loop.
- * Return - The logic returns 1 if the code appends and 0 if the code has an error.
- * */
-
+/**
+ * append_text_to_file - Appends text to an existing file.
+ * @filename: The name of the file to which text will be appended.
+ * @text_content: The text to append to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-  int file_D, a, b = 0;
+	int file_D, a, b = 0;
 
-  if (!filename)
-	  return (-1);
-  file_D = open(filename, O_WRONLY | O_APPEND);
-  if (file_D < 0)
-	  return (-1);
-  if (text_content)
-  {
-    while (text_content[b])
-	    b++;
-    a = write(file_D, text_content, b);
-    if (a != b)
-	    return (-1);
-  }
-   close(file_D);
+	if (!filename)
+		return (-1);
 
-   return (1);
+	file_D = open(filename, O_WRONLY | O_APPEND);
+	if (file_D < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[b])
+			b++;
+
+		a = write(file_D, text_content, b);
+		if (a != b)
+			return (-1);
+	}
+
+	close(file_D);
+
+	return (1);
 }
+
